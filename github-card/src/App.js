5e78108e7a55b7c2d21a3a7d0a7df94a92595c44
Trevor Thompson <trevthom18@gmail.com>
2019-12-15 13:32:14 -0500

    Committing basics of Axios retrieval calls

--- github-card/src/App.js
+++ github-card/src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+       dataSet : [],
+       followerDataset: []
+    }
+  }
+
+  componentDidMount() {
+
+    axios
+       .get("https://api.github.com/users/tthompson2")
+
+
+       .then(response => {
+            console.log(response);
+
+            this.setState({
+              dataSet : response.data
+            })
+       })
+
+       .catch(error => {
+
+           console.log(error);
+       })
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Here is my information
+        </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+        </a>
+        </header>
+      </div>
+    );
+  }
+
+}
 export default App;
